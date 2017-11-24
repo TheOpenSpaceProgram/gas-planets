c15f540f81c3ae5a682a28be02880beaae7b6ed1
190n <ben@190n.org>
2017-11-24 14:48:56 -0800

    Remove object files and other build artifacts from src

--- src/build_info.h
+++ src/build_info.h
@@ -3,7 +3,7 @@
 
 /* This file is machine generated */
 #define BUILD_INFO_STRING1 "v. " SNIS_VERSION " - 64-bit little-endian x86_64 GNU/Linux"
-#define BUILD_INFO_STRING2 "SNIS009 6ea1cfeb3ae9f32f9436247b2ec496cad1bcfa53 + 1285"
+#define BUILD_INFO_STRING2 "SNIS009 fc8b0e02e7b1fa06d24ac81e7e1064d6145ee728 + 1244"
 #define BUILD_INFO_STRING3 "SPACE NERDS IN SPACE v. " SNIS_VERSION
 
 #endif
