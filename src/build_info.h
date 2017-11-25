5ea75d409390a5236f5bc55a37b0da6f8be245c7
190n <ben@190n.org>
2017-11-25 09:12:52 -0800

    Apply patch to fix Makefile and remove build_info.h

--- src/build_info.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BUILD_INFO
-#define __BUILD_INFO
-
-/* This file is machine generated */
-#define BUILD_INFO_STRING1 "v. " SNIS_VERSION " - 64-bit little-endian x86_64 GNU/Linux"
-#define BUILD_INFO_STRING2 "SNIS009 fc8b0e02e7b1fa06d24ac81e7e1064d6145ee728 + 1244"
-#define BUILD_INFO_STRING3 "SPACE NERDS IN SPACE v. " SNIS_VERSION
-
-#endif
